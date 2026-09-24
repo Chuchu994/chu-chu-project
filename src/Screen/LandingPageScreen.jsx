e0aab003aff23383e5b23390992761e827295cec
Chuchu994 <chuchu2chuchu94@gmail.com>
2026-09-24 13:58:49 -0700

    update New-Job-React

--- src/Screen/LandingPageScreen.jsx
+++ src/Screen/LandingPageScreen.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Header from "../component/Header/Header";
 import Hero from "../component/Header/Hero/Hero";
 import About from "../component/Header/About/About";
 import Testimony from "../component/Header/Testimony/Testimony";
@@ -8,7 +7,6 @@ import Cta from "../component/Header/Cta/Cta";
 const LandingPageScreen = () => {
   return (
     <div>
-        <Header /> 
       <Hero />
       <About /> 
       <Testimony />  
